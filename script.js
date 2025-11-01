f6ac49b0b4f2411a8e27b509b689d8ed858b64d6
Kevin Shen <kevinshenhs@gmail.com>
2025-11-01 19:48:07 -0400

    css overhaul

--- script.js
+++ script.js
@@ -32,3 +32,18 @@ window.addEventListener("scroll", () => {
 
   lastScrollY = currentScrollY;
 });
+
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      }
+    });
+  },
+  { threshold: 0.2 }
+);
+
+sections.forEach((sec) => observer.observe(sec));
