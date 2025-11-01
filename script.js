d21b7b8da9b4f8e9f2caaf631804a987588f3bed
Kevin Shen <kevinshenhs@gmail.com>
2025-11-01 13:25:28 -0400

    added js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// Smooth scroll for internal links
+
+// Smooth scroll
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
@@ -6,4 +7,28 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
       behavior: "smooth",
     });
   });
+});
+
+// Header scroll effect
+let lastScrollY = window.scrollY;
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+  const currentScrollY = window.scrollY;
+
+  if (currentScrollY > 100) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+
+  if (currentScrollY > lastScrollY && currentScrollY > 200) {
+    // Scrolling down
+    header.classList.add("hide");
+  } else {
+    // Scrolling up
+    header.classList.remove("hide");
+  }
+
+  lastScrollY = currentScrollY;
 });
